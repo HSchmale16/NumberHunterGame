60cf58a6acd0109846c22fdd5322f81fd14cd78b
Henry <hschmale16@gmail.com>
2015-01-22 19:59:09 -0500

    Worked on particle system. Last commit for tonight.

--- include/ParticleSystem.h
+++ include/ParticleSystem.h
@@ -7,17 +7,31 @@
  */
 
 #include <SFML/Graphics.hpp>
-
+#include <vector>
 
 class ParticleSystem: sf::Drawable
 {
 	public:
-		ParticleSystem();
+		ParticleSystem(int count);
 		virtual ~ParticleSystem();
 
 		void update();
+		void setEmitter(float x, float y);
 	protected:
 	private:
+		struct Particle{
+      sf::Vector2f m_veloc;
+      int m_currLife;
+      int m_maxLife;
+		};
+
+		virtual void draw(sf::RenderTarget &target, sf::RenderStates states);
+		void resetParticle(int index);
+
+		int m_particlesCount;								//!< Number of Particles
+		std::vector<Particle> m_particles;	//!< Particles
+		sf::VertexArray m_vertexArray;			//!< Vertexes
+		sf::Vector2f m_emiterLocal;					//!< Location of Emitter
 };
 
 #endif // PARTICLESYSTEM_H
--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "16";
+	static const char DATE[] = "22";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.01";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 0;
-	static const long BUILD  = 972;
-	static const long REVISION  = 7639;
+	static const long BUILD  = 978;
+	static const long REVISION  = 7906;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2020;
-	#define RC_FILEVERSION 0,0,972,7639
-	#define RC_FILEVERSION_STRING "0, 0, 972, 7639\0"
-	static const char FULLVERSION_STRING [] = "0.0.972.7639";
+	static const long BUILDS_COUNT  = 2035;
+	#define RC_FILEVERSION 0,0,978,7906
+	#define RC_FILEVERSION_STRING "0, 0, 978, 7906\0"
+	static const char FULLVERSION_STRING [] = "0.0.978.7906";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 972;
+	static const long BUILD_HISTORY  = 978;
 	
 
 }
