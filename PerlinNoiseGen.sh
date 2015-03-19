d4bffdc9e8fc6b0eab3d622dea6f052c0f194591
Henry J Schmale <hschmale16@gmail.com>
2015-03-19 17:07:46 -0400

    Reduced Generation Size of Perlin Data File
    * But Still Massive, Will Reduce to 3 digit random numbers

--- PerlinNoiseGen.sh
+++ PerlinNoiseGen.sh
@@ -4,21 +4,32 @@
 # Perlin Noise Initial Vector Generator
 # Generates the data to use in Dynamic Background Generator System
 
+TEN=10 # Bash is really annoying about not using straight up numeric constants
+
 echo '{' # Opening Brace
 for y in {0..999} ; do
     echo '    {'
     for x in {0..999}; do
-        echo -n '        {'
+        let "l = $x % 5"
+        if [ "$l" -eq 1 ] ; then
+            echo -n '        {'
+        else
+            echo -n '{'
+        fi
         for a in 0 1 ; do
             # Write the actual Numbers Here
             v=$[100 + (RANDOM % 100)]$[1000 + (RANDOM % 1000)]
-            v=0.${v:1:2}${v:4:3}
+            v=0.${v:1:2}${v:4}
             echo -n $v
             if [ $a -ne 1 ] ; then
                 echo -n ', '
             fi
         done
-        echo '},'
+        if [ $l -eq 0 ] ; then
+            echo '},'
+        else
+            echo -n '},'
+        fi
     done
     if [ $y -ne 999 ] ; then
         echo '    },'
