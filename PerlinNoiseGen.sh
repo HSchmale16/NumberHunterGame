981ccb6c9db8bce6159fedddd486b455289a0a6f
Henry J Schmale <hschmale16@gmail.com>
2015-03-19 16:34:31 -0400

    Perlin Noise Generator Script Works
    However the file it generates in kind of bloated
    
    \todo Make more effective use of LOCs and Bytes in Generated Data

--- PerlinNoiseGen.sh
+++ PerlinNoiseGen.sh
@@ -4,10 +4,26 @@
 # Perlin Noise Initial Vector Generator
 # Generates the data to use in Dynamic Background Generator System
 
-for y in {0..1000} ; do
-    for x in {0..1000}; do
+echo '{' # Opening Brace
+for y in {0..999} ; do
+    echo '    {'
+    for x in {0..999}; do
+        echo -n '        {'
         for a in 0 1 ; do
-            echo $y $x $a
+            # Write the actual Numbers Here
+            v=$[100 + (RANDOM % 100)]$[1000 + (RANDOM % 1000)]
+            v=0.${v:1:2}${v:4:3}
+            echo -n $v
+            if [ $a -ne 1 ] ; then
+                echo -n ', '
+            fi
         done
+        echo '},'
     done
+    if [ $y -ne 999 ] ; then
+        echo '    },'
+    else
+        echo '    }'
+    fi
 done
+echo '};'  # Closing Brace
