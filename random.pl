494230e0273aa90d7eb5e04598012030d6b6e3df
Henry J Schmale <hschmale16@gmail.com>
2015-03-22 20:07:22 -0400

    Wrote A Perl Script
    Wrote a very short perl script for generating random numbers
    in the perlin vector generation script

---
+++ random.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# Henry J Schmale
+# March 22, 2015
+# Random Number Generator For Perlin Vector Generator Bash Script
+
+use strict;
+use warnings;
+
+my $RndNum = rand(2) - 1.0;
+print $RndNum . "\n";
