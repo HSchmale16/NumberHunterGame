43e26ea8abb3723c5e52ba64c7da3bf298669e23
Henry <hschmale16@gmail.com>
2015-03-25 16:34:54 -0400

    Documentation
    Extended Documentation of my standard library, which is by now something
    I would call terrible, but it is too deeply embedded now to make changes
    or replace it with something else.

--- Hjs_StdLib_Implement.cpp
+++ Hjs_StdLib_Implement.cpp
@@ -1,16 +1,22 @@
-/** Implementation of my StdLib
-*   This has no class implementation just function implementation
-*   @author Henry Schmale
-*   @date Sept 25, 2014
-*/
+/** @brief  Implementation of my StdLib
+ *  @author Henry J Schmale
+ *  @date   Sept 25, 2014
+ *  @file   Hjs_StdLib_Impelement.cpp
+ *
+ *  This has no class implementation just the implementation of functions
+ *  defined in `Hjs_StdLib.h`. These are mostly general functions for
+ *  checking various things, and logging. The logging functionallity is
+ *  removed if `DEBUG_BUILD` is not defined, as it speeds up execution of
+ *  the program, because `cout`/`cerr` is extremely slow on MS Windows.
+ */
 
 #include "Hjs_StdLib.h"
 #include <math.h>
 
 /// Variables that are for internal use by Hjs_StdLib
-bool bLibIsInit = false;        // Is the lib init'd
-time_t startTime;               // time in program execution lib was init'd
-bool bMainThreadActive = true;  // Is the main thread still active
+time_t startTime;                 // time in program execution lib was init'd
+bool   bLibIsInit = false;        // Is the lib init'd
+bool   bMainThreadActive = true;  // Is the main thread still active
 
 /// Initiates my library
 void hjs::init_Hjs_StdLib()
--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1162;
-	static const long REVISION  = 7213;
+	static const long BUILD  = 1164;
+	static const long REVISION  = 7355;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2399;
-	#define RC_FILEVERSION 0,1,1162,7213
-	#define RC_FILEVERSION_STRING "0, 1, 1162, 7213\0"
-	static const char FULLVERSION_STRING [] = "0.1.1162.7213";
+	static const long BUILDS_COUNT  = 2403;
+	#define RC_FILEVERSION 0,1,1164,7355
+	#define RC_FILEVERSION_STRING "0, 1, 1164, 7355\0"
+	static const char FULLVERSION_STRING [] = "0.1.1164.7355";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 162;
+	static const long BUILD_HISTORY  = 164;
 	
 
 }
