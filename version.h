a27f8fc1aae95f0a4df948b753b0b75ee3eae753
Henry <hschmale16@gmail.com>
2015-03-31 12:32:48 -0400

    Flipping
    * Cloud Tile Size
    * Tile Transparency
    * Flipping of cloud Tile

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1267;
-	static const long REVISION  = 2761;
+	static const long BUILD  = 1269;
+	static const long REVISION  = 2850;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2590;
-	#define RC_FILEVERSION 0,1,1267,2761
-	#define RC_FILEVERSION_STRING "0, 1, 1267, 2761\0"
-	static const char FULLVERSION_STRING [] = "0.1.1267.2761";
+	static const long BUILDS_COUNT  = 2604;
+	#define RC_FILEVERSION 0,1,1269,2850
+	#define RC_FILEVERSION_STRING "0, 1, 1269, 2850\0"
+	static const char FULLVERSION_STRING [] = "0.1.1269.2850";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 267;
+	static const long BUILD_HISTORY  = 269;
 	
 
 }
