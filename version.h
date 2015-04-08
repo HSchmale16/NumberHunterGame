517423022aa253b3b590c2abfca4dddee3e3cf65
Henry <hschmale16@gmail.com>
2015-04-08 11:58:21 -0400

    The merge wasn't right
    * Bunch of crap got mixed into filepaths.h, when merging
      Now it's fixed

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "02";
+	static const char DATE[] = "08";
 	static const char MONTH[] = "04";
 	static const char YEAR[] = "2015";
 	static const char UBUNTU_VERSION_STYLE[] =  "15.04";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1282;
-	static const long REVISION  = 3544;
+	static const long BUILD  = 1283;
+	static const long REVISION  = 3603;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2622;
-	#define RC_FILEVERSION 0,1,1282,3544
-	#define RC_FILEVERSION_STRING "0, 1, 1282, 3544\0"
-	static const char FULLVERSION_STRING [] = "0.1.1282.3544";
+	static const long BUILDS_COUNT  = 2625;
+	#define RC_FILEVERSION 0,1,1283,3603
+	#define RC_FILEVERSION_STRING "0, 1, 1283, 3603\0"
+	static const char FULLVERSION_STRING [] = "0.1.1283.3603";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 282;
+	static const long BUILD_HISTORY  = 283;
 	
 
 }
