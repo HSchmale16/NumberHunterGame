8103e34ac886621e9001325a7ecbcd7127a256f3
Henry <hschmale16@gmail.com>
2015-04-26 11:45:52 -0400

    Removed Additional Cruft from the source code
    * Remove SQLite References from gameDB code
    \todo Remove SQLite linking from project file

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 1347;
-	static const long REVISION  = 6770;
+	static const long BUILD  = 1350;
+	static const long REVISION  = 7005;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 2782;
-	#define RC_FILEVERSION 0,1,1347,6770
-	#define RC_FILEVERSION_STRING "0, 1, 1347, 6770\0"
-	static const char FULLVERSION_STRING [] = "0.1.1347.6770";
+	static const long BUILDS_COUNT  = 2792;
+	#define RC_FILEVERSION 0,1,1350,7005
+	#define RC_FILEVERSION_STRING "0, 1, 1350, 7005\0"
+	static const char FULLVERSION_STRING [] = "0.1.1350.7005";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 347;
+	static const long BUILD_HISTORY  = 350;
 	
 
 }
